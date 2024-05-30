e01b48720d893a148dd8c778c14da16a7c2699b3
Mukul yadav <mukulydv14@gmail.com>
2024-05-31 03:04:21 +0530

    Deploy server fixed

--- deploy-server/index.js
+++ deploy-server/index.js
@@ -8,20 +8,26 @@ const startProcess = async () => {
   const appName = process.env.APP_NAME;
   const chart = process.env.CHART;
   const deploymentId = process.env.DEPLOYMENT_ID;
+  const helmRepo = process.env.HELM_REPO;
 
   try {
     await createNamespace(namespace);
-    await helmInstall(appName, namespace, chart);
+    await helmInstall(helmRepo, appName, namespace, chart);
   } catch (error) {
     console.log("Deployment error:", error);
-    await fetch("http://localhost:5000/deploy/error", {
-      method: "POST",
+    // await fetch("http://localhost:5000/deploy/error", {
+    //   method: "POST",
 
-      body: JSON.stringify({
-        deploymentId,
-      }),
-    });
+    //   body: JSON.stringify({
+    //     deploymentId,
+    //   }),
+    // });
   }
+
+  // Keep the process running
+  // setInterval(() => {
+  //   console.log("Process is running...");
+  // }, 60000); // log every minute
 
   process.exit(0);
 };
--- deploy-server/utils/helmClient.js
+++ deploy-server/utils/helmClient.js
@@ -1,9 +1,13 @@
 const { exec } = require("child_process");
 
-const helmInstall = (releaseName, namespace, chart) => {
+const helmInstall = (helmRepo, releaseName, namespace, chart) => {
   return new Promise((resolve, reject) => {
+    console.log(
+      `helm repo add ${releaseName} ${helmRepo} && helm install ${releaseName} ${releaseName}/${chart} --namespace ${namespace}`
+    );
     exec(
-      `helm install ${releaseName} ${chart} --namespace ${namespace}`,
+      // "kubectl get namespace",
+      `helm repo add ${releaseName} ${helmRepo} && helm install ${releaseName} ${releaseName}/${chart} --namespace ${namespace}`,
       (error, stdout, stderr) => {
         if (error) {
           console.error(`helm install error: ${stderr}`);
